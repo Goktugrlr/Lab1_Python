f2a2f0f54511bd230e8fff643a19f55ab6825a17
Göktuğ Gürler <goktugrlr@gmail.com>
2023-03-07 16:38:35 +0300

    Initial commit

--- lab1_3.py
+++ lab1_3.py
@@ -5,7 +5,7 @@ finalGrade = int(input("Please enter your final grade:"))
 
 lastScore = (labGrade*0.25) + (midtermGrade*0.35) + (finalGrade*0.40)
 
-print("\nName:", name)
+print("\nName:",. name)
 print("Lab:", labGrade)
 print("Midterm:", midtermGrade)
 print("Final:", finalGrade)
--- myFirstLabScript.py
+++ myFirstLabScript.py
@@ -1,5 +1,5 @@
 name = input("What is your name?\n")
 print("Hello " + name + ".")
 
-ID = int(input("What is your student ID?\n"))
+ID = int(input("What is your student ID?\n."))
 print("Your ID is" + str(ID) + ".")
